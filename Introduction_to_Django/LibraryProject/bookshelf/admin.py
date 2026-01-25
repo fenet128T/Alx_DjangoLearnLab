4df02e2bb12c7c0642ff1bb02377385ede5fa4e5
fenet128t <fenet128t@gmail.com>
2026-01-25 16:05:36 +0300

    Restoring Introduction_to_Django folder structure

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+# Customizing the Admin Interface
+class BookAdmin(admin.ModelAdmin):
+    # Display these fields in the admin list view
+    list_display = ('title', 'author', 'publication_year')
+    
+    # Add search capabilities for title and author
+    search_fields = ('title', 'author')
+    
+    # Add filters for the publication year
+    list_filter = ('publication_year',)
+
+# Register the model with the custom admin class
+admin.site.register(Book, BookAdmin)

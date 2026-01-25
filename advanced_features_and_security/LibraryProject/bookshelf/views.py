9f44b2c5b921df2b2ce992eba925f856b00af1cd
fenet128t <fenet128t@gmail.com>
2026-01-25 23:28:52 +0300

    Implement security best practices: CSRF, Secure Settings, and SQLi protection1

---
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import permission_required
+from .models import Book
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    # Edit logic here
+    return render(request, 'bookshelf/edit_book.html', {'book': book})
+   
--- advanced_features_and_security/LibraryProject/relationship_app/views.py
+++ advanced_features_and_security/LibraryProject/relationship_app/views.py
@@ -39,17 +39,6 @@ def delete_book(request, pk):
 
 
 
-@permission_required('bookshelf.can_view', raise_exception=True)
-def book_list(request):
-    books = Book.objects.all()
-    return render(request, 'bookshelf/book_list.html', {'books': books})
-
-@permission_required('bookshelf.can_edit', raise_exception=True)
-def edit_book(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    # Edit logic here
-    return render(request, 'bookshelf/edit_book.html', {'book': book})
-
 
 # Role-specific views
 @user_passes_test(is_admin)

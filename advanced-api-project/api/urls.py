d28d8aeb03b0a1a8b7350e16eebbdc936c99d81d
fenet128t <fenet128t@gmail.com>
2026-02-02 23:35:19 +0300

    fix: configuration of the url parameters

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
     path('books/create/', BookCreateView.as_view(), name='book-create'),
     path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+    path('books/delete', BookDeleteView.as_view(), name='book-delete'),
 ]

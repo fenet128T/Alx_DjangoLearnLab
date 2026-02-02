ae4924ca47ac71ec25796579713137be1fb22fa6
fenet128t <fenet128t@gmail.com>
2026-02-02 23:51:21 +0300

    fix: views import

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,5 @@
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from django_filters.rest_framework import DjangoFilterBackend
-
+from django_filters import rest_framework
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import generics, permissions, filters

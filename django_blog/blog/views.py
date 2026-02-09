ac5bba330e18483b824dba172ba2274a35052fb6
fenet128t <fenet128t@gmail.com>
2026-02-10 01:40:03 +0300

    fix:comment create form

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -152,6 +152,24 @@ def add_comment(request, pk):
 
     return redirect('post-detail', pk=post.pk)
 
+# ----------------- CREATE COMMENT ----------------
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'blog/comment_form.html'
+
+    def form_valid(self, form):
+        post_id = self.kwargs['pk']  # get post id from URL
+        post = get_object_or_404(Post, pk=post_id)
+        form.instance.post = post
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return self.object.post.get_absolute_url()
+
+
+
 # ----------------- UPDATE COMMENT ----------------
 class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Comment

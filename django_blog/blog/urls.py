d227066e77059abe9991216b3783754ca8b26d99
fenet128t <fenet128t@gmail.com>
2026-02-10 01:43:32 +0300

    fix:comment's post

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -30,9 +30,8 @@ urlpatterns = [
     path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
     path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
 
-
     # Comment URLs
-    path("post/<int:pk>/comment/new/", add_comment, name="add-comment"),
+    path("post/<int:pk>/comments/new/", add_comment, name="add-comment"),
     path("comment/<int:pk>/update/", CommentUpdateView.as_view(), name="comment-update"),
     path("comment/<int:pk>/delete/", CommentDeleteView.as_view(), name="comment-delete"),
 

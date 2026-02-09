40f943c8c56ac52a37bd0146839d3628c3911067
fenet128t <fenet128t@gmail.com>
2026-02-10 00:29:56 +0300

    create:post form

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Post
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
@@ -9,3 +10,13 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+
+class PostForm(forms.ModelForm):
+    """
+    Form for creating and updating blog posts.
+    The author field is excluded because it will be set automatically.
+    """
+
+    class Meta:
+        model = Post
+        fields = ["title", "content"]

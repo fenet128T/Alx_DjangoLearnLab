5691192181b9cfb5828d689ec21e578998400c13
fenet128t <fenet128t@gmail.com>
2026-02-11 20:50:40 +0300

    fix: TagWidget

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Post,Tag
 from .models import Comment
+from taggit.forms import TagWidget
 
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
@@ -23,7 +24,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ["title", "content", "tags"]
-
+        widgets = {
+            'tags': TagWidget(),  
+        }
 
 class CommentForm(forms.ModelForm):
     class Meta:

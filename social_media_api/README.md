8002918f4152b8e66256f9c22a31af4a14118d24
fenet128t <fenet128t@gmail.com>
2026-02-18 01:03:11 +0300

    Configure project for production deployment with Gunicorn and WhiteNoise

--- social_media_api/README.md
+++ social_media_api/README.md
@@ -12,6 +12,8 @@ Search: Built-in filtering to search posts by title or content.
 Pagination: Results are paginated (10 per page) to ensure high performance.
 
 
+
+
 Social Media API - Follows & Feed:::
 
 This update introduces user-to-user relationships and a personalized content feed.
@@ -22,3 +24,28 @@ Follow System: Users can follow/unfollow others to curate their network.
 Dynamic Feed: A personalized list of posts from followed users, sorted by the most recent.
 
 Self-Follow Protection: Logic prevents users from following their own accounts.
+
+
+
+
+
+Deployment Guide:::
+This document outlines the steps taken to deploy the Social Media API to a production environment.
+
+🚀 Production Configuration
+Hosting Provider: Render / Heroku
+
+Web Server: Gunicorn (WSGI)
+
+Database: PostgreSQL (Managed Service)
+
+Static Files: Managed via WhiteNoise and collectstatic.
+
+🛠️ Security Checklist
+[x] DEBUG set to False.
+
+[x] SECRET_KEY pulled from environment variables.
+
+[x] Security headers (HSTS, XSS Filter) enabled.
+
+[x] HTTPS redirection enforced.
--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,11 +24,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-in#(z4xy7o_)-o8mu^n-tag%d=!0^(scq=ndwhpcuek*%e5tz0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pythonanywhere.com']
 
-
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_SSL_REDIRECT = True
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-in#(z4xy7o_)-o8mu^n-tag%d=!0^(scq=ndwhpcuek*%e5tz0')
 # Application definition
 
 INSTALLED_APPS = [
@@ -46,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,3 +135,5 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 AUTH_USER_MODEL = 'accounts.CustomUser'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'

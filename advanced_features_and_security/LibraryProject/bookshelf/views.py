391db7fd301bdcb862100b442d41e0b236e0eb94
fenet128t <fenet128t@gmail.com>
2026-02-18 02:56:53 +0300

    update bookshelfs forms

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import permission_required
 from .models import Book
+from .forms import ExampleForm
+
+def form_example_view(request):
+    form = ExampleForm()
+    return render(request, 'bookshelf/form_example.html', {'form': form})
 
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
